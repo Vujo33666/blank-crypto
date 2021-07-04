6ed34a7c042a5ca58b4c8d5c936f23399b2f9dfd
Marko Vujnovic <markovujnovic3@gmail.com>
2021-07-04 21:04:30 +0200

    First task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ethereum.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,29 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Welcome to my Blank Crypto Internship project!
+        </p>
+        <div className="App-link-container">
+          <a
+            className="App-link"
+            href="https://github.com/Vujo33666"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My GitHub
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/marko-vujnovi%C4%87-95b4a4214/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My Linkedin
+          </a>
+        </div>
       </header>
     </div>
   );
