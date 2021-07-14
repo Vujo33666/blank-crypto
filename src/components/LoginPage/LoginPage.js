b35de9d187f488528be9d89c0c423bdbc720e0ae
Marko Vujnovic <markovujnovic3@gmail.com>
2021-07-14 17:16:22 +0200

    adding styles to login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,14 +1,24 @@
 import React, {useState,useEffect} from "react";
 import {BrowserRouter as Router, Switch, Route, Redirect, useHistory} from "react-router-dom";
+import {Button, TextField, makeStyles} from '@material-ui/core';
 import logo from "../../ethereum.svg"
+import login from "../../images/login.svg"
+import circle from "../../images/circle.png"
 import auth from '../../auth';
 import styles from './style.module.css'
 import Cookies from "js-cookie";
 
+const useStyles = makeStyles({
+    root:{
+          margin: "10px auto 10px",
+          textTransform: "none"
+    }
+});
+
 const LoginPage = (props) =>{
 
     let history=useHistory();
-
+    const classes=useStyles();
     const [address,setAddress] = useState("");
 
     function handleAddress(value){
@@ -27,23 +37,33 @@ const LoginPage = (props) =>{
     },[])*/
 
     return (
-        <div>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2 className={styles.heading}>Login page</h2>
-            <input 
-                name="address"
-                placeholder="Enter your ETH address..."
-                type="text"
-                value={address}
-                onChange={(event)=>handleAddress(event.target.value)}>
-            </input>
-            <button
-                onClick={()=>{
-                    
-                   auth.login(()=>{
-                        props.history.push("/dashboard");
-                    });
-                }}>Login button</button>
+        <div className={styles.main_container}>
+            <img src={login} className={styles.login} alt="login"/>
+            <div className={styles.container}>
+                <img src={logo} className={styles.logo} alt="logo" />
+                <h2 className={styles.heading}>Login</h2>
+                <TextField 
+                    id="outlined-basic"
+                    label="Your address"
+                    variant="outlined"
+                    className={[styles.fields,classes.root].join(" ")}
+                    name="address"
+                    type="text"
+                    value={address}
+                    onChange={(event)=>handleAddress(event.target.value)}>
+                </TextField>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.root}
+                    onClick={()=>{
+                        
+                    auth.login(()=>{
+                            props.history.push("/dashboard");
+                        });
+                    }}>Sign in
+                </Button>
+            </div>
         </div>
     );
 }
